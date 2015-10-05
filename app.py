ef6d69620ccc46bedf3f1b91e8e0af9886f09c29
Vladimir <volkvid@yandex.ru>
2015-10-05 17:53:13 +0300

    init blueprints and connection

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 __author__ = 'vladimir'
 
 from flask import Flask
+import pymysql
+
+
+connection = pymysql.connect(
+    host='localhost',
+    user='user',
+    password='passwd',
+    db='db',
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+
+app = Flask("db-api")
 
 
 if __name__ == "__main__":
