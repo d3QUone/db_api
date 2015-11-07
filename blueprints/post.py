661284857564491c5fa0c618e09220cff09f8dd1
Vladimir <volkvid@yandex.ru>
2015-11-08 01:38:36 +0300

    added post post methods

--- blueprints/post.py
+++ blueprints/post.py
@@ -18,7 +18,7 @@ def create():
         params = request.json
         # print "*"*50, "\nPost.Create got params: {0}\n".format(repr(params)), "*"*50
     except Exception:
-        print "thread.create params exception:\n{0}".format(traceback.format_exc())
+        print "post.create params exception:\n{0}".format(traceback.format_exc())
         return ujson.dumps({"code": c_BAD_REQUEST, "response": "invalid json"})
     # required
     date = params.get("date", None)
@@ -34,28 +34,176 @@ def create():
     isSpam = int(bool(params.get("isSpam", None)))
     isDeleted = int(bool(params.get("isDeleted", None)))
     if date and thread and thread > 0 and message and user and forum:
-        p_id = update_query(
-            "INSERT INTO `post` (`date`, `thread`, `message`, `user`, `forum`, `parent`, `isApproved`, `isHighlighted`, `isEdited`, `isSpam`, `isDeleted`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-            (date, thread, message, user, forum, parent, isApproved, isHighlighted, isEdited, isSpam, isDeleted),
+        thread_obj = select_query(
+            "SELECT t.`posts` FROM `thread` t WHERE t.`id` = %s",
+            (thread, ),
             verbose=False
         )
-        post = {
-            "id": p_id,
-            "message": message,
-            "date": date,  # datetime.date.strftime()
-            "thread": thread,
-            "user": user,
-            "parent": parent,
-            "isApproved": isApproved,
-            "isDeleted": isDeleted,
-            "isEdited": isEdited,
-            "isSpam": isSpam,
-            "isHighlighted": isHighlighted,
-            "forum": forum,
-        }
-        code = c_OK
+        if len(thread_obj) == 1:
+            p_id = update_query(
+                "INSERT INTO `post` (`date`, `thread`, `message`, `user`, `forum`, `parent`, `isApproved`, `isHighlighted`, `isEdited`, `isSpam`, `isDeleted`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                (date, thread, message, user, forum, parent, isApproved, isHighlighted, isEdited, isSpam, isDeleted),
+                verbose=False
+            )
+            post = {
+                "id": p_id,
+                "message": message,
+                "date": date,
+                "thread": thread,
+                "user": user,
+                "parent": parent,
+                "isApproved": isApproved,
+                "isDeleted": isDeleted,
+                "isEdited": isEdited,
+                "isSpam": isSpam,
+                "isHighlighted": isHighlighted,
+                "forum": forum,
+            }
+            # update post amount in thread ...
+            thread_obj[0]["posts"] += 1
+            update_query(
+                "UPDATE `thread` SET `posts` = %s WHERE `id` = %s",
+                (thread_obj[0]["posts"], thread),
+                verbose=False
+            )
+            code = c_OK
+        else:
+            post = "No such thread"
+            code = c_NOT_FOUND
     else:
         post = "Invalid parameters passed"
+        code = c_INVALID_REQUEST_PARAMS
+    return ujson.dumps({
+        "response": post,
+        "code": code,
+    })
+
+
+@post_blueprint.route("/update/", methods=["POST"])
+def update():
+    try:
+        params = request.json
+    except Exception:
+        print "post.update params exception:\n{0}".format(traceback.format_exc())
+        return ujson.dumps({"code": c_BAD_REQUEST, "response": "invalid json"})
+    # required
+    post_id = get_int_or_none(params.get("post", None))
+    message = params.get("message", None)
+    if post_id and post_id > 0 and message:
+        post_query = select_query(
+            "SELECT * from `post` p WHERE p.`id` = %s",
+            (post_id, ),
+            verbose=False
+        )
+        if len(post_query) == 1:
+            update_query(
+                "UPDATE `post` SET `message` = %s WHERE `id` = %s",
+                (message, post_id),
+                verbose=False
+            )
+            post = post_query[0]
+            post["date"] = get_date(post["date"])
+            post["message"] = message
+            code = c_OK
+        else:
+            post = "This post doesn't exist"
+            code = c_NOT_FOUND
+    else:
+        post = "Invalid params"
+        code = c_INVALID_REQUEST_PARAMS
+    return ujson.dumps({
+        "response": post,
+        "code": code,
+    })
+
+
+@post_blueprint.route("/remove/", methods=["POST"])
+def remove():
+    try:
+        params = request.json
+    except Exception:
+        print "post.remove params exception:\n{0}".format(traceback.format_exc())
+        return ujson.dumps({"code": c_BAD_REQUEST, "response": "invalid json"})
+    post_id = get_int_or_none(params.get("post", None))
+    if post_id and post_id > 0:
+        post_query = select_query(
+            "SELECT p.`thread` FROM `post` p WHERE p`isDeleted` = FALSE AND p.`id` = %s",
+            (post_id, ),
+            verbose=False
+        )
+        if len(post_query) == 1:
+            update_query(
+                "UPDATE `post` SET `isDeleted` = 0 WHERE `id` = %s",
+                (post_id, ),
+                verbose=False
+            )
+            # update post amount in thread ...
+            thread = post_query[0]["thread"]
+            thread_obj = select_query(
+                "SELECT t.`posts` FROM `thread` t WHERE t.`id` = %s",
+                (thread, ),
+                verbose=False
+            )
+            thread_obj[0]["posts"] -= 1
+            update_query(
+                "UPDATE `thread` SET `posts` = %s WHERE `id` = %s",
+                (thread_obj[0]["posts"], thread),
+                verbose=False
+            )
+            post = {"post": post_id}
+            code = c_OK
+        else:
+            post = "This post doesn't exist"
+            code = c_NOT_FOUND
+    else:
+        post = "Invalid params"
+        code = c_INVALID_REQUEST_PARAMS
+    return ujson.dumps({
+        "response": post,
+        "code": code,
+    })
+
+
+@post_blueprint.route("/restore/", methods=["POST"])
+def restore():
+    try:
+        params = request.json
+    except Exception:
+        print "post.restore params exception:\n{0}".format(traceback.format_exc())
+        return ujson.dumps({"code": c_BAD_REQUEST, "response": "invalid json"})
+    post_id = get_int_or_none(params.get("post", None))
+    if post_id and post_id > 0:
+        post_query = select_query(
+            "SELECT p.`thread` FROM `post` p WHERE p`isDeleted` = 1 AND p.`id` = %s",
+            (post_id, ),
+            verbose=False
+        )
+        if len(post_query) == 1:
+            update_query(
+                "UPDATE `post` SET `isDeleted` = 0 WHERE `id` = %s",
+                (post_id, ),
+                verbose=False
+            )
+            # update post amount in thread ...
+            thread = post_query[0]["thread"]
+            thread_obj = select_query(
+                "SELECT t.`posts` FROM `thread` t WHERE t.`id` = %s",
+                (thread, ),
+                verbose=False
+            )
+            thread_obj[0]["posts"] += 1
+            update_query(
+                "UPDATE `thread` SET `posts` = %s WHERE `id` = %s",
+                (thread_obj[0]["posts"], thread),
+                verbose=False
+            )
+            post = {"post": post_id}
+            code = c_OK
+        else:
+            post = "This post doesn't exist"
+            code = c_NOT_FOUND
+    else:
+        post = "Invalid params"
         code = c_INVALID_REQUEST_PARAMS
     return ujson.dumps({
         "response": post,
@@ -67,15 +215,15 @@ def create():
 def detail():
     post_id = get_int_or_none(request.args.get("post", None))
     related = request.values.getlist("related")
-    if post_id:
+    if post_id and post_id > 0:
         # simple query
-        post = select_query(
-            "SELECT * FROM `post` p WHERE p.`id` = %s AND p.`isDeleted` = FALSE",
+        post_query = select_query(
+            "SELECT * FROM `post` p WHERE p.`id` = %s",  # AND p.`isDeleted` = FALSE
             (post_id, ),
             verbose=False
         )
-        if len(post) >= 1:
-            post = post[0]
+        if len(post_query) == 1:
+            post = post_query[0]
             post["date"] = get_date(post["date"])
             code = c_OK
         else:
@@ -83,6 +231,54 @@ def detail():
             code = c_NOT_FOUND
     else:
         post = "Invalid params"
+        code = c_INVALID_REQUEST_PARAMS
+    return ujson.dumps({
+        "response": post,
+        "code": code,
+    })
+
+
+@post_blueprint.route("/vote/", methods=["POST"])
+def vote_post():
+    try:
+        params = request.json
+    except Exception:
+        print "post.vote params exception:\n{0}".format(traceback.format_exc())
+        return ujson.dumps({"code": c_BAD_REQUEST, "response": "invalid json"})
+    post_id = get_int_or_none(params.get("post", None))
+    vote = get_int_or_none(params.get("vote", None))
+    if post_id and post_id > 0 and vote:
+        post_query = select_query(
+            "SELECT * FROM `post` p WHERE p.`id` = %s",
+            (post_id, ),
+            verbose=False
+        )
+        if len(post_query) == 1:
+            post = post_query[0]
+            if vote == 1:
+                post["likes"] += 1
+                update_query(
+                    "UPDATE `post` p SET p.`likes` = %s WHERE p.`id` = %s",
+                    (post["likes"], post_id, ),
+                    verbose=False
+                )
+                code = c_OK
+            elif vote == -1:
+                post["dislikes"] += 1
+                update_query(
+                    "UPDATE `post` p SET p.`dislikes` = %s WHERE p.`id` = %s",
+                    (post["dislikes"], post_id, ),
+                    verbose=False
+                )
+                code = c_OK
+            else:
+                post = "Invalid params passed"
+                code = c_INVALID_REQUEST_PARAMS
+        else:
+            post = "Requested post not found"
+            code = c_NOT_FOUND
+    else:
+        post = "Invalid params passed"
         code = c_INVALID_REQUEST_PARAMS
     return ujson.dumps({
         "response": post,
